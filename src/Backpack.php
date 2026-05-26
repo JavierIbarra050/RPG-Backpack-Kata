241a6c322b6e248ae8640ff7d858ceb63bdf06e3
JavierIbarra050 <ibarra.161804@e.unavarra.es>
2026-05-26 18:48:14 +0200

    [GREEN] - Implementar equipar N objetos devuelve esa cantidad de ese objeto

--- src/Backpack.php
+++ src/Backpack.php
@@ -4,6 +4,7 @@ namespace Deg540\CleanCodeKata9;
 
 class Backpack
 {
+    private $contenidosBackpack = [];
     public function gestionarBackpack(string $accion): string
     {
         if(!$accion) return "";
@@ -17,6 +18,12 @@ class Backpack
             $cantidad = $accion[2];
         }
 
-        return $objeto . " x" . $cantidad;
+        $this->contenidosBackpack[$objeto] = (int) $cantidad;
+
+        foreach ($this->contenidosBackpack as $clave => $valor) {
+            return $clave . ' x' . $valor;
+        }
+
+        return "";
     }
 }
