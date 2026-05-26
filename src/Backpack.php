32f012dbb7ad0083e595d42e8ff4e6a4c865467f
JavierIbarra050 <ibarra.161804@e.unavarra.es>
2026-05-26 19:01:31 +0200

    [GREEN] - Implementar dada acción desequipar devolver desequipar

--- src/Backpack.php
+++ src/Backpack.php
@@ -4,32 +4,40 @@ namespace Deg540\CleanCodeKata9;
 
 class Backpack
 {
-    private $contenidosBackpack = [];
+    private array $contenidosBackpack = [];
     public function gestionarBackpack(string $accion): string
     {
         if(!$accion) return "";
 
         $accion = explode(" ", $accion);
 
-        $objeto = $accion[1];
-        $cantidad = 1;
-
-        if(count($accion) === 3){
-            $cantidad = $accion[2];
-        }
-
-        if(!isset($this->contenidosBackpack[$objeto]))
+        if($accion[0] == "desequipar")
         {
-            $this->contenidosBackpack[$objeto] = (int) $cantidad;
+            return "desequipar";
         }
         else
         {
-            $this->contenidosBackpack[$objeto] += (int) $cantidad;
-        }
+            $objeto = $accion[1];
+            $cantidad = 1;
 
-        $objetosMochilaEnString = [];
-        foreach ($this->contenidosBackpack as $clave => $valor) {
-            $objetosMochilaEnString[] = $clave . ' x' . $valor;
+            if(count($accion) === 3){
+                $cantidad = $accion[2];
+            }
+
+            if(!isset($this->contenidosBackpack[$objeto]))
+            {
+                $this->contenidosBackpack[$objeto] = (int) $cantidad;
+            }
+            else
+            {
+                $this->contenidosBackpack[$objeto] += (int) $cantidad;
+            }
+
+            $objetosMochilaEnString = [];
+            foreach ($this->contenidosBackpack as $clave => $valor)
+            {
+                $objetosMochilaEnString[] = $clave . ' x' . $valor;
+            }
         }
 
         return implode(" - ", $objetosMochilaEnString);
